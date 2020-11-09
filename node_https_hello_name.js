33a2496b4f0ca74d35597a0acb77389dfbe4b158
Mayank Choubey <mayank.choubey@ushur.com>
2020-11-08 22:27:05 -0800

    .

---
+++ node_https_hello_name.js
@@ -0,0 +1,19 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync(process.env.KEY_FILE, 'utf8'),
+    cert: fs.readFileSync(process.env.CERT_FILE, 'utf8')
+};
+  
+var server = https.createServer(options, (request, response) => {
+    let data = '';
+    request.on('data', chunk => data += chunk);
+    request.on('end', () => {
+        response.writeHead(200, {"Content-Type": "application/json"});
+        response.end(JSON.stringify({name: JSON.parse(data).name}));
+    });
+});
+
+server.listen(process.env.PORT||3000);
+
