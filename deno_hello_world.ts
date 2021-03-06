5a537708e43a497e255dcf2118a97c5c4a031e60
Mayank Choubey <mayank.choubey@ushur.com>
2021-03-06 14:42:31 -0800

    .

--- deno_hello_world.ts
+++ deno_hello_world.ts
@@ -1,10 +1,40 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 
-const text = await Deno.readTextFile("/var/tmp/sample.json");
-const json = JSON.parse(text);
+const get1 = JSON.parse(await Deno.readTextFile("/Users/mayankc/Work/source/deno-vs-nodejs/getData.json"));
+const get2 = JSON.parse(await Deno.readTextFile("/Users/mayankc/Work/source/deno-vs-nodejs/getData2.json"));
+const post1 = JSON.parse(await Deno.readTextFile("/Users/mayankc/Work/source/deno-vs-nodejs/postData.json"));
 
 const s = serve({ port: 3000 });
 for await (const req of s) {
-  req.respond({ status: 200,
-                body: JSON.stringify(json) });
+    console.log(req.url);
+    switch(req.method) {
+        case 'GET': {
+            switch(req.url) {
+                case '/': {
+                    req.respond({   status: 200,
+                                    body: JSON.stringify(get1) });
+                    break;
+                }
+
+                case '/another': {
+                    req.respond({   status: 200,
+                                    body: JSON.stringify(get2) });
+                    break;
+                }
+            }
+
+            break;
+        }
+
+        case 'POST': {
+            switch(req.url) {
+                case '/': {
+                    req.respond({   status: 200,
+                                    body: JSON.stringify(post1) });
+                    break;
+                }
+            }
+        }
+    }
+
 }
--- deno_hello_world_oak_exp.ts
+++ deno_hello_world_oak_exp.ts
@@ -1,11 +1,23 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-const text = await Deno.readTextFile("/var/tmp/sample.json");
-const json = JSON.parse(text);
+const get1 = JSON.parse(await Deno.readTextFile("/Users/mayankc/Work/source/deno-vs-nodejs/getData.json"));
+const get2 = JSON.parse(await Deno.readTextFile("/Users/mayankc/Work/source/deno-vs-nodejs/getData2.json"));
+const post1 = JSON.parse(await Deno.readTextFile("/Users/mayankc/Work/source/deno-vs-nodejs/postData.json"));
+
+const router = new Router();
+router
+    .get('/', ctx => {
+        ctx.response.body=get1;
+    })
+    .get('/another', ctx => {
+        ctx.response.body=get2;
+    })
+    .post('/', ctx => {
+        ctx.response.body=post1;
+    });
 
 const app = new Application();
-app.use((ctx) => {
-  ctx.response.body = json;
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 await app.listen({ port: 3000 });
